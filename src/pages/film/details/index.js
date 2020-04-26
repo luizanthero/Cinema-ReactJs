c7236419ea5a6e536f3f3669225ab3f23b8381d1
Luiz Anthero Gama <luizanthero@icloud.com>
2020-04-26 15:46:38 -0300

    ajust film info detalis

--- src/pages/film/details/index.js
+++ src/pages/film/details/index.js
@@ -9,13 +9,14 @@ export default class Film extends Component {
     film: {},
     filmOmdb: {},
     apiKey: "?apikey=f2396906",
+    plotFilm: "&plot=full",
   };
 
   async componentDidMount() {
     const { id } = this.props.match.params;
     const response = await cineAPi.get("/films/" + id);
     const responseOmdb = await apiOmdb.get(
-      this.state.apiKey + "&i=" + response.data.ApiCode
+      this.state.apiKey + this.state.plotFilm + "&i=" + response.data.ApiCode
     );
     this.setState({ film: response.data, filmOmdb: responseOmdb.data });
   }
@@ -27,15 +28,51 @@ export default class Film extends Component {
       <div className="show-box show-box-info">
         <h1>{film.Name}</h1>
 
-        <img src={filmOmdb.Poster} alt={film.Name} title={film.Name} />
+        <div className="show-box-info-image">
+          <img src={filmOmdb.Poster} alt={film.Name} title={film.Name} />
+          <div className="show-box-info-info">
+            <p>
+              <strong>Duração:</strong> {filmOmdb.Runtime}
+            </p>
+            <p>
+              <strong>Tipo:</strong> {filmOmdb.Type}
+            </p>
+            <p>
+              <strong>Ano:</strong> {filmOmdb.Year}
+            </p>
+            <p>
+              <strong>Gênero:</strong> {filmOmdb.Genre}
+            </p>
+            <p>
+              <strong>Produção:</strong> {filmOmdb.Production}
+            </p>
+            <p>
+              <strong>Diretor:</strong> {filmOmdb.Director}
+            </p>
+            <p>
+              <strong>Idiomas:</strong> {filmOmdb.Language}
+            </p>
+            <p>
+              <strong>Paises:</strong> {filmOmdb.Country}
+            </p>
+            <p>
+              <strong>Prêmios:</strong> {filmOmdb.Awards}
+            </p>
+            <p>
+              <strong>DVD:</strong> {filmOmdb.DVD}
+            </p>
+            <p>
+              <strong>Nota IMDb:</strong> {filmOmdb.imdbRating}
+            </p>
+            <p>
+              <strong>Votos:</strong> {filmOmdb.imdbVotes}
+            </p>
+          </div>
+        </div>
 
-        <p>Ano: {filmOmdb.Year}</p>
-        <p>Gênero: {filmOmdb.Genre}</p>
-        <p>Diretor: {filmOmdb.Director}</p>
-        <p>Idiomas: {filmOmdb.Language}</p>
-        <p>Resumo: {filmOmdb.Plot}</p>
-        <p>Paises: {filmOmdb.Country}</p>
-        <p>Prêmios: {filmOmdb.Awards}</p>
+        <div className="show-box-info-resume">
+          <p>{filmOmdb.Plot}</p>
+        </div>
 
         <Link to="/films" className="button button-default button-back">
           Voltar
