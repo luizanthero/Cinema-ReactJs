12a5d1a6852429981e640ec4a3a46648383f6bf9
Luiz Anthero Gama <luizanthero@icloud.com>
2020-04-26 00:01:56 -0300

    add sidebar menu

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,6 +1,28 @@
-import React from "react";
+import React, { Component } from "react";
+import IconButton from "@material-ui/core/IconButton";
+import MenuIcon from "@material-ui/icons/Menu";
+
 import "./styles.css";
 
-const Header = () => <header id="main-header">CineAPI</header>;
+import Sidebar from "../Sidebar";
 
-export default Header;
+export default class Header extends Component {
+  state = {
+    isActive: false,
+  };
+
+  render() {
+    return (
+      <>
+        <header id="main-header">
+          <IconButton edge="start" color="inherit" aria-label="menu">
+            <MenuIcon />
+          </IconButton>
+        </header>
+        <>
+          <Sidebar />
+        </>
+      </>
+    );
+  }
+}
---
+++ src/components/Sidebar/index.js
@@ -0,0 +1,21 @@
+import React from "react";
+import "./styles.css";
+
+const Sidebar = () => (
+  <ul id="main-menu">
+    <li>
+      <a href="teste.html">CineAPI</a>
+    </li>
+    <li>
+      <a href="teste.html">Filmes</a>
+    </li>
+    <li>
+      <a href="teste.html">Salas</a>
+    </li>
+    <li>
+      <a href="teste.html">Telas</a>
+    </li>
+  </ul>
+);
+
+export default Sidebar;
